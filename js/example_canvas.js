723f5ad1cd1bdbf0b663433a24c841dc8bcebb6c
eranboudjnah <shadowcra@gmail.com>
2019-03-08 13:59:03 +0000

    Removed automatic start

--- js/example_canvas.js
+++ js/example_canvas.js
@@ -1,64 +1,48 @@
 function startClustering() {
 	$('#startButton').hide();
 
-	var POINT_RADIUS = 1;
-	var FULL_CIRCLE = Math.PI * 2;
-	var clustersDIV = $('#clusters');
-	var pointsCanvas = $('<canvas></canvas>');
-	var pointsDIV = $('#points');
-	pointsDIV.append(pointsCanvas);
-	pointsCanvas
-		.css({ width: pointsDIV.width(), height: pointsDIV.height() })
-		.attr({width: pointsDIV.width(), height: pointsDIV.height() });
-	console.log(pointsCanvas);
-	var pointsContext = pointsCanvas[0].getContext("2d");
-	var colors = ['#FF0000', '#00FF00', '#0000FF', '#FF00FF', '#33AA88', '#FF8800', '#0088FF', '#003333', '#AA00AA', '#8800FF'];
+	var canvas;
+	var colors = [
+		"#FF0000", "#00FF00", "#0000FF", "#FF00FF",
+		"#33AA88", "#FF8800", "#0088FF", "#003333",
+		"#AA00AA", "#8800FF"
+	];
 
-  cluster.start({
-		newClusterLabel: function (clusterID) {
-			var div = $('<DIV></DIV>');
-			div
-        .css({ position: 'absolute',
-                display: 'block',
-                  color: colors[clusterID]
-             })
-        .text(clusterID + 1)
-        .attr('id', 'c' + clusterID);
+	var drawPoint = function(x, y, color) {
+		context.clearRect(x - 1, y - 1, 2, 2);
+		context.beginPath();
+		context.rect(x - 1, y - 1, 2, 2);
+		context.fillStyle = color;
+		context.fill();
+		context.closePath();
+	};
 
-      clustersDIV.append(div);
+	var canvasTag = $("<canvas><\/canvas>");
+	canvasTag.attr("id", "clusteringCanvas");
+	var clustersDiv = $("#clusters");
+	clustersDiv.append(canvasTag);
+	canvas = canvasTag[0];
+	var context = canvas.getContext('2d');
+	context.canvas.height = clustersDiv.height();
+	context.canvas.width = clustersDiv.width();
 
-			return {
-				div: div,
-				set: function(point, title) {
-					this.div
-						.css({ left: Math.round(point.x), top: Math.round(point.y) })
-						.text(title);
-				}
-			};
+	cluster.start({
+		colors: colors,
+		itemsCount: 20000,
+		interval: 1,
+		newItemEntityFunction: function (initialClusterId, itemId, item, cache) {
+			drawPoint(item.x, item.y, colors[initialClusterId % colors.length]);
 		},
-
-		newItem: function (point, clusterID) {
-			return {
-				point: point,
-				setCluster: function(clusterID) {
-					pointsContext.beginPath();
-					pointsContext.arc(this.point.x + .5, this.point.y + .5, POINT_RADIUS, 0, FULL_CIRCLE);
-					pointsContext.fillStyle = colors[clusterID];
-					pointsContext.fill();
-				}
-			};
+		updateItemEntityFunction: function (item) {
+			drawPoint(item.x, item.y, colors[item.clusterId % colors.length]);
 		},
-
-		willUpdateItems: function () {
-			console.log('Clearing')
-			pointsContext.clearRect(0, 0, pointsCanvas.width, pointsCanvas.height);
-		},
-
-		updatedItems: function () {
-		},
-
-		onComplete: function (timeElapsed, steps) {
-			setTimeout(function () { alert('Done in ' + timeElapsed + ' seconds.\nSteps: ' + steps); }, 100);
+		randomFunction: (function() {
+			return Math.round(Math.random() * 600) + 2
+		}),
+		beforeStepFunction: function (step, isLastStep) {
+		if (isLastStep) {
+			context.clearRect(0, 0, canvas.width, canvas.height);
+		}
 		}
 	});
 }
